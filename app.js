086b5e859dd9c7db565576f00d88ae123c7b400e
andy-risdiyanto <andy.risdiyanto@gmail.com>
2024-08-27 14:44:18 +0700

    Update Lagi App JS

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/about', (req, res) => {
 
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
-  connection.query(sql,(error, fields) => {
+  connection.query(sql,(fields) => {
      res.send(fields)
     })
 });
