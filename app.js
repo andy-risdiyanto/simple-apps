45ee5380af620537a29ac5d94f15a40a1764616b
andy-risdiyanto <andy.risdiyanto@gmail.com>
2024-08-27 14:49:53 +0700

    Update Posisi Code App JS

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.get('/about', (req, res) => {
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(fields) => {
-     res.send(fields)
-    })
+  res.send(fields)
+   })
 });
 
 app.listen(process.env.APP_PORT, () => {
